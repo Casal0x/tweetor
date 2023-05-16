aaae66701a38c47602b5756ed3f0ff70c0d114f2
Sebastian Casal <sebastian.casal@gmail.com>
2023-05-16 19:37:13 -0300

    fixing CreatePostWizzard

--- src/components/Posts/CreatePostWizzard.tsx
+++ src/components/Posts/CreatePostWizzard.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { useAuth, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
 import { api } from "~/utils/api";
-import LoadingSpinner from "../Base/LoadingSpinner";
+import LoadingSpinner from "~/components/Base/LoadingSpinner";
 
 const CreatePostWizzard: React.FC = () => {
   const { user } = useUser();
-  const { signOut } = useAuth();
+  const { data: profile } = api.profile.getProfileById.useQuery();
+
   const [content, setContent] = React.useState("");
   const ctx = api.useContext();
 
@@ -31,96 +30,21 @@ const CreatePostWizzard: React.FC = () => {
     }
   };
 
-  const handleSignOut = () => {
-    signOut().catch(console.log);
-  };
-
-  if (!user) return null;
+  if (!user || !profile) return null;
 
   return (
     <div className="mx-6 my-6 flex w-full pt-10 sm:pt-0 ">
-      <Menu as="div" className="relative inline-block">
-        <div>
-          <Menu.Button className="relative h-20 w-20">
-            <Image
-              src={user.profileImageUrl}
-              alt={user.fullName || ""}
-              fill
-              sizes="100vw"
-              className="rounded-full"
-              style={{
-                objectFit: "contain",
-              }}
-            />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-slate-900 shadow-lg ring-1 ring-white ring-opacity-10 focus:outline-none">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <Link
-                    href={`/profile/${user.id}`}
-                    className={`${
-                      active ? "bg-violet-500 text-white" : "text-white"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <i className="fa-regular fa-user mr-2" />
-                    ) : (
-                      <i className="fa-regular fa-user mr-2" />
-                    )}
-                    Profile
-                  </Link>
-                )}
-              </Menu.Item>
-
-              <Menu.Item>
-                {({ active }) => (
-                  <Link
-                    href={`/profile/settings`}
-                    className={`${
-                      active ? "bg-violet-500 text-white" : "text-white"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <i className="fa-solid fa-gear mr-2" />
-                    ) : (
-                      <i className="fa-solid fa-gear mr-2" />
-                    )}
-                    Settings
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-violet-500 text-white" : "text-white"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    onClick={handleSignOut}
-                  >
-                    {active ? (
-                      <i className="fa-solid fa-right-from-bracket mr-2" />
-                    ) : (
-                      <i className="fa-solid fa-right-from-bracket mr-2" />
-                    )}
-                    Log out
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
+      <div className="relative h-20 w-20">
+        <Link href={`@${profile.username}`}>
+          <Image
+            src={profile.profileImageUrl}
+            className="mt-2 cursor-pointer rounded-full"
+            alt={`@${profile?.username || "unknown"}'s profile picture`}
+            width={130}
+            height={130}
+          />
+        </Link>
+      </div>
 
       <form onSubmit={onSubmit} className="flex h-full w-full flex-col">
         <input
